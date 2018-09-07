41ec5351e349da1c86c92411a587c6dd138ec3cd
Tuomo Nieminen <tuomo.nieminen@thl.fi>
2018-09-07 15:45:03 +0300

    Revert "stop if sample data not found"
    this reverts commit bccf2d122ddd385d7b08a1180cb1dec8492f10c6.

--- R/gt3x_sample_data.R
+++ R/gt3x_sample_data.R
@@ -8,9 +8,6 @@
 #' @export
 gt3x_datapath <- function(index = NULL) {
   datadir <- file.path(path.package("read.gt3x"), "extdata")
-  if(!dir.exists(datadir)) stop("Sample data not found")
-  # TODO: download data from remote source the first time this is called.
-
   if(!is.null(index)) {
     files <- list_gt3x(datadir)
     if(index > length(files)) stop("Index is larger than the number of sample files, which there are ", length(files))
