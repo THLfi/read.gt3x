1031cda3b53729f22dcb71c33fa24aa2134c1411
Tuomo Nieminen <tuomo.nieminen@thl.fi>
2018-09-07 15:49:48 +0300

    stop if data directory does not exist

--- R/gt3x_sample_data.R
+++ R/gt3x_sample_data.R
@@ -8,6 +8,8 @@
 #' @export
 gt3x_datapath <- function(index = NULL) {
   datadir <- file.path(path.package("read.gt3x"), "extdata")
+  if(!dir.exists(datadir)) stop("Data directory not found")
+  # TODO: download data the first time this is called.
   if(!is.null(index)) {
     files <- list_gt3x(datadir)
     if(index > length(files)) stop("Index is larger than the number of sample files, which there are ", length(files))
