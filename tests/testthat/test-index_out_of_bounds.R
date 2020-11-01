7a88a699c5ddd8d1bedace7630c1db058f52c108
muschellij2 <muschellij2@gmail.com>
2020-11-01 13:16:48 -0500

    trying to fix #18

--- tests/testthat/test-index_out_of_bounds.R
+++ tests/testthat/test-index_out_of_bounds.R
@@ -17,3 +17,20 @@ test_that("Index out of bounds no longer an issue", {
     abs(hdr$`Last Sample Time` -  res$time[nrow(res)]) < 1/hdr$`Sample Rate`
   )
 })
+
+
+test_that("Negative Missing values  fixed", {
+  # Fixes https://github.com/THLfi/read.gt3x/issues/18
+  # check
+  testthat::skip_on_cran()
+
+  destfile = tempfile(fileext = ".gt3x.gz")
+  curl::curl_download("https://ndownloader.figshare.com/files/24319343",
+                      destfile = destfile, quiet = FALSE)
+  x = read.gt3x(destfile)
+  at = attributes(x)
+  head(at$missingness)
+  testthat::expect_false(any(at$missingness$n_missing < 0))
+  x = read.gt3x(destfile, imputeZeroes = TRUE, asDataFrame = TRUE)
+})
+
