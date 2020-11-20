8a33ce35e395430d8dae34a74527ff47187bc35d
muschellij2 <muschellij2@gmail.com>
2020-11-20 13:15:40 -0500

    added in the dates to ticks options

--- R/utils.R
+++ R/utils.R
@@ -38,10 +38,10 @@ unzip_zipped_gt3x <- function(path, cleanup = TRUE) {
     zip_exts <- exts[unzip_these & fe]
     zip_outfiles <- mapply(function(x, y) {
       FUN <- switch(y,
-                   bz = bzfile,
-                   bz2 = bzfile,
-                   gz = gzfile,
-                   xz = xzfile)
+                    bz = bzfile,
+                    bz2 = bzfile,
+                    gz = gzfile,
+                    xz = xzfile)
       R.utils::decompressFile(
         x,
         ext = y,
@@ -130,13 +130,50 @@ have_log <- function(path, verbose = TRUE) {
 #' reference:
 #' \url{https://stackoverflow.com/questions/35240874/r-net-ticks-to-timestamp-in-r}
 #' @param ticks values in NET ticks format
+#' @param x values in date-time format coerced to ticks
 #' @param tz timezone, passed to \code{\link{as.POSIXct}}
 #' @family gt3x-utils
+#' @examples
+#' mystr = "599633592352500000"
+#' x = read.gt3x:::ticks2datetime(mystr)
+#' x
+#' out = read.gt3x:::datetime2ticks(as.POSIXct("1901-03-02 08:40:35.25", tz = "UTC"))
+#' out = as.character(out)
+#' out
+#' stopifnot(out == mystr)
+#'
 ticks2datetime <- function(ticks, tz = "GMT") {
-  ticks <- as.numeric(ticks)
+  ticks <- as.double(ticks)
   seconds <- ticks / 1e7
   datetime <- as.POSIXct(seconds, origin = "0001-01-01", tz = tz)
   datetime
+}
+
+#' @family gt3x-utils
+#' @rdname ticks2datetime
+datetime2ticks <- function(x) {
+  timezone = attr(x, "tzone")
+  if (!timezone %in% c("GMT", "UTC")) {
+    warning(
+      paste0(
+        "date time object not in UTC/GMT, should use ",
+        "lubridate::with_tz or lubridate::tz to change before making ticks")
+    )
+  }
+  days = difftime(as.Date(x), as.Date("0001-01-01"), units = "days")
+  days = as.double(days)
+  seconds = difftime(x,
+                     as.POSIXct(paste0(as.Date(x), " 00:00:00"), tz = timezone),
+                     units = "secs")
+  seconds = as.double(seconds)
+  seconds = round(seconds, 3)
+
+  ticks = days * 86400 + seconds
+  ticks = round(ticks * 1000)
+  # need it because of overflow
+  ticks = format(ticks, scientific = FALSE)
+  ticks = paste0(ticks, "0000")
+  ticks
 }
 
 #' Calculate the expected activity sample size from start time
