a471720fcb53c06f35b05fc94040c2a5cc9238c3
muschellij2 <muschellij2@gmail.com>
2020-11-20 15:51:27 -0500

    updated memory

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,14 @@
 library(testthat)
 library(read.gt3x)
 
-memory.limit(100000)
+if (.Platform$OS.type == "windows") {
+  is_64 = grepl("x86_64", R.version$arch)
+  if (is_64) {
+    limit = 10000
+  } else {
+    limit = 4000
+  }
+  memory.limit(max(limit, memory.limit()))
+}
 
 test_check("read.gt3x")
