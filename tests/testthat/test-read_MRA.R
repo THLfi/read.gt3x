c27f9f4b7ef7185b6184845ddbf094727d3b7d5a
E. G. Patrick Bos <egpbos@gmail.com>
2021-11-05 11:50:07 +0100

    Fix read_MRA test
    It was failing because it couldn't find destroy_field. This was defined
    one level up in testthat.R, but apparently this is not in scope for the
    actual test file.

--- tests/testthat/test-read_MRA.R
+++ tests/testthat/test-read_MRA.R
@@ -3,6 +3,40 @@ url <- paste0("https://ndownloader.figshare.com/files/25557302")
 path <- tempfile(fileext = ".gt3x.gz")
 dl <- utils::download.file(url, destfile = path, mode = "wb")
 
+destroy_field = function(path, field = "Acceleration Scale",
+                         replace_field = list()) {
+  path <- read.gt3x:::unzip_zipped_gt3x(path, cleanup = TRUE)
+
+  tdir = tempfile()
+  out = unzip(path, exdir = tdir, overwrite = TRUE)
+  info_file = out[grepl("info.txt", out)]
+
+  if (all(field %in% "all")) {
+    out = out[!grepl("info.txt", out)]
+  } else {
+    info = readLines(info_file)
+    info
+    keys = sapply(strsplit(info, split = ":"), `[`, 1)
+    info = info[!keys %in% field]
+    if (length(replace_field) >0) {
+      for (ifield in seq_along(replace_field)) {
+        n =  names(replace_field)[ifield]
+        ind = which(keys == n)
+        info[ind] = paste0(n, ": ", replace_field[[ifield]])
+      }
+    }
+    writeLines(info, info_file)
+  }
+  new_gt3x_file = tempfile(fileext = ".gt3x")
+  if (requireNamespace("zip", quietly = TRUE)) {
+    zip::zip(files = basename(out),
+             zipfile = new_gt3x_file,
+             root = tdir,
+             include_directories = FALSE)
+  }
+  return(new_gt3x_file)
+}
+
 testthat::test_that("Reading in Old MRA", {
   testthat::expect_error({
     res <- read.gt3x::read.gt3x(path = character(0))
