368382e30b5861bc1b56fcdf6f7a51eee5f6ec17
muschellij2 <muschellij2@gmail.com>
2026-01-27 15:29:09 -0500

    added in filling zeros

--- R/fill_zeros.R
+++ R/fill_zeros.R
@@ -16,6 +16,11 @@ fill_zeros = function(x) {
   x$Y = vctrs::vec_fill_missing(x$Y, direction = "down")
   x$Z = vctrs::vec_fill_missing(x$Z, direction = "down")
 
+
+  x$X[is.na(x$X)] = 0
+  x$Y[is.na(x$Y)] = 0
+  x$Z[is.na(x$Z)] = 0
+
   x
 }
 
